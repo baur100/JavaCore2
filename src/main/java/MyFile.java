14e184e3a3e1eddc33d3b2392f154f7d908360ee
Nikolay Vakolyuk <nvakoliouk@gmail.com>
2020-03-25 20:38:14 -0700

    lesson6

--- src/main/java/MyFile.java
+++ src/main/java/MyFile.java
@@ -1,20 +1,25 @@
 public class MyFile {
     public static void main(String[] args) {
-        System.out.println("Hello, Github");
-        int res = mult3(3, 3, 5);
-        System.out.println(res);
+
+        //System.out.println("Hello, Github");
+
+        //int res = mult3(3, 3, 5);
+        //System.out.println(res);
+
         int price = 260;
         int age1 = 32;
         int age2 = 30;
         int age3 = 68;
         int age4 = 5;
         double age5 = 1.8;
+
         System.out.println("pers1 = " + ticketPrice(age1, price));
         System.out.println("pers2 = " + ticketPrice(age2, price));
         System.out.println("pers3 = " + ticketPrice(age3, price));
         System.out.println("pers4 = " + ticketPrice(age4, price));
         System.out.println("pers5 = " + ticketPrice(age5, price));
     }
+
     public static double ticketPrice(double age, double price) {
         double ticketPrice = price;
         if (age < 2) {
@@ -31,8 +36,9 @@ public class MyFile {
         }
         return ticketPrice;
     }
-    public static int mult3(int a, int b, int c) {
-        int x = a * b * c;
-        return x;
-    }
+
+    //public static int mult3(int a, int b, int c) {
+    //   int x = a * b * c;
+    //   return x;
+    //}
 }
