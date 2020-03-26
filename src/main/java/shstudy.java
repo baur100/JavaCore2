f5b4dfa987e7089b241664fb25a7d55f69f98f41
Sergey Smith <shsmith@bk.ru>
2020-03-25 23:05:54 -0400

    l7

---
+++ src/main/java/shstudy.java
@@ -0,0 +1,16 @@
+public class shstudy {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Github" + mult3(2,5,4));
+
+    }
+    public static int mult3(int a, int b, int c)  {
+        int x =a*b*c;
+        return x;
+    }
+
+    public static double ticketPrice(double age, double price){
+
+
+    }
+}
