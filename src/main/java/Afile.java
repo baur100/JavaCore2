2cfb3c8612e8e1f3a3fbcb407a8d1f06049e7320
Alyona T <onehappywoman0509@gmail.com>
2020-03-25 20:20:22 -0700

    all

---
+++ src/main/java/Afile.java
@@ -0,0 +1,15 @@
+public class Afile {
+    public static void main(String[]args){
+        System.out.println("Hello, Github!");
+        int res=mult3(3,3,5);
+        System.out.println(res);
+        int price=260;
+
+        int age1=32;
+        int age2=30;
+        int age3=68;
+        int age4=5;
+        double age5=1.8;
+        System.out.println("pers1=");
+    }
+}
