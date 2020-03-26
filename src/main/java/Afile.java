22e34d8600e81cee8410c2e7bf8cacc1e5654304
Alyona T <onehappywoman0509@gmail.com>
2020-03-25 20:38:14 -0700

    all

--- src/main/java/Afile.java
+++ src/main/java/Afile.java
@@ -1,8 +1,5 @@
 public class Afile {
     public static void main(String[]args){
-        System.out.println("Hello, Github!");
-        int res=mult3(3,3,5);
-        System.out.println(res);
         int price=260;
 
         int age1=32;
@@ -11,6 +8,26 @@ public class Afile {
         int age4=5;
         double age5=1.8;
         System.out.println("pers1="+tPrice(age1, price));
-        System.out.println(());
+        System.out.println("pers2="+tPrice(age2, price));
+        System.out.println("pers3="+tPrice(age3, price));
+        System.out.println("pers4="+tPrice(age4, price));
+        System.out.println("pers5="+tPrice(age5, price));
+    }
+    static double tPrice(double age, double price) {
+        double tPrice = price;
+        if (age < 2) {
+            tPrice = 0;
+        }
+        if (age >= 2 && age <= 12) {
+            tPrice = price * 0.5;
+        }
+        if (age > 12 && age < 66) {
+            tPrice = price;
+        }
+        if (age >= 66) {
+            tPrice = price * 0.8;
+        }
+        return tPrice;
     }
 }
+
