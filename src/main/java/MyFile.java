b9c5e9e1bf49f92e8e6b3e912a1e7add6ea50d66
Nas Kas <nasu792@gmail.com>
2020-03-25 22:10:37 -0500

    L7

--- src/main/java/MyFile.java
+++ src/main/java/MyFile.java
@@ -10,14 +10,17 @@ public class MyFile {
         int age3=68;
         int age4=5;
         double age5=1.8;
-        System.out.println("pers1="+ticketPrice((age1,price));
+        System.out.println("pers1="+ticketPrice(age1,price));
         System.out.println("pers2="+ticketPrice((age2,price));
-        System.out.println("pers3="+ticketPrice((age3,price));
-        System.out.println("pers4="+ticketPrice((age4,price));
-        System.out.println("pers5="+ticketPrice((age5,price));
+        System.out.println("pers3="+ticketPrice(age3,price));
+        System.out.println("pers4="+ticketPrice(age4,price));
+        System.out.println("pers5="+ticketPrice(age5,price));
 
     }
-
+    public static int mult3 ( int a, int b, int c){
+        int x = a * b * c;
+        return x;
+    }
     public static double ticketPrice (double age, double price) {
         double ticketPrice = price;
         if (age < 2) {
@@ -35,9 +38,6 @@ public class MyFile {
         return ticketPrice;
 
 
-        public static int mult3 ( int a, int b, int c){
-            int x = a * b * c;
-            return x;
-        }
+
     }
 }
