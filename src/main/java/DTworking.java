d55854cbe2e0552f62ea902cf4f67b42dbe8106e
torchilkin dim <dauzhuk@gmail.com>
2020-03-27 12:33:04 -0400

    hw7

--- src/main/java/DTworking.java
+++ src/main/java/DTworking.java
@@ -1,40 +1,20 @@
 public class DTworking {
     public static void main(String[] args) {
-        int res1 = mult1(1,2,3);
-        System.out.println(res1);
-        int age1=32;
-        int age2=30;
-        int age3=68;
-
-        int age4=5;
-
-
-
-
-
-
+      int res=min(1,2,3,4,5,6);
+        System.out.println(res);
 
     }
-    public static int mult1(int a, int s, int d){
-        int i = a*s*d;
-        return i;
-    }
-    public static double ticketprice(double age, double price){
-        double ticketprice = price;
-        if(age<2){
-            ticketprice=0;
-        }
-        if(age>=2&&age<12){
-            ticketprice= price *0.5;
-        }
-        if(age>=12&&age<66){
-            ticketprice= price;
-        }
-        if(age<+66) {
-            ticketprice = price * 0.8;
-        }
-        return price;
 
+    public static int min(int a, int b, int c, int d, int e, int f) {
+        int[] arr = new int[]{a, b, c, d, e, f};
+        int min =arr[0];
+         for(int v:arr){
+        if (v < min) {
+            min = v;
+        }
     }
+     return min;
+    }
+
 
 }
---
+++ src/main/java/homework7/HW7.java
@@ -0,0 +1,53 @@
+package homework7;
+
+import javax.swing.*;
+
+public class HW7 {
+    public static void main(String[] args) {
+        System.out.println("***SUM***");
+        int res1 = sum(2, 3, 5, 6, 7, 8);
+        System.out.println(res1);
+        System.out.println("***SORT***");
+
+
+
+        System.out.println("***MIN***");
+        int resmin = min(3, 9, 1, 8, 4, 8, 5);
+        System.out.println(resmin);
+        System.out.println("***MAX***");
+        int res2 = max(3, 9, 1, 18, 4, 8, 5);
+        System.out.println(res2);
+
+    }
+
+    public static int sum(int a, int b, int c, int d, int e, int f) {
+        int sum = a + b + c + d + e + f;
+        return sum;
+    }
+
+
+
+
+    public static int min(int a, int b, int c, int d, int e, int f, int g) {
+        int[] arr = new int[]{a, b, c, d, e, f, g};
+        int min = arr[0];
+        for (int v : arr) {
+            if (v < min) {
+                min = v;
+            }
+                   }
+        return min;
+    }
+
+
+    public static int max(int a, int b, int c, int d, int e, int f, int g) {
+        int[] arr = new int[]{a, b, c, d, e, g};
+        int max = arr[0];
+        for (int v : arr) {
+            if (v > max) {
+                max = v;
+            }
+        }
+        return max;
+    }
+}
