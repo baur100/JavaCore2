ab09cbc0162f8f2530b002ff71e53174ba9cc158
Nikolay Vakolyuk <nvakoliouk@gmail.com>
2020-03-26 22:31:05 -0700

    hw8

---
+++ src/main/java/hw7/hw7_1.java
@@ -0,0 +1,15 @@
+package hw7;
+
+public class hw7_1 {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 6, 7, 8};
+        int sum = 0;
+        for(int i = 0; i < arr.length; i ++) {
+            sum += arr[i];
+        }
+       return  sum;
+        System.out.println(arr[i]);
+
+        }
+    }
+}
---
+++ src/main/java/hw7/hw7_3.java
@@ -0,0 +1,4 @@
+package hw7;
+
+public class hw7_3 {
+}
