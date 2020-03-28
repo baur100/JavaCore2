039028671b05712ab2ecd58ff4a50f28683e509e
torchilkin dim <dauzhuk@gmail.com>
2020-03-27 22:49:22 -0400

    hw7- check

--- src/main/java/DTworking.java
+++ src/main/java/DTworking.java
@@ -1,20 +1,47 @@
+import java.util.Arrays;
+
 public class DTworking {
     public static void main(String[] args) {
-      int res=min(1,2,3,4,5,6);
-        System.out.println(res);
+      int []arr1={1,5,6,7,8,9,4};
+        System.out.println(sum(arr1));
+       sort (arr1);
 
     }
-
-    public static int min(int a, int b, int c, int d, int e, int f) {
-        int[] arr = new int[]{a, b, c, d, e, f};
-        int min =arr[0];
-         for(int v:arr){
-        if (v < min) {
-            min = v;
+    public static int sum(int[]arr){
+        int sum =0;
+        for(int v:arr){
+            sum=sum+v;
         }
+        return sum;
     }
-     return min;
+    public static void sort(int[]arr){
+        Arrays.sort(arr);
+        for(int v:arr){
+            System.out.print(v+" ");
+
+        }
+
+
     }
+    public static int min_max(int[]arr) {
+        int min = arr[0];
+        int max = arr[0];
+        for (int v : arr) {
+            if (v > max) {
+                max = v;
+            }
+            if (v < min) {
+                min = v;
+            }
+        }
+        int[] result = {min, max};
+        return result;       }
+
+
+
+
+
+
 
 
 }
