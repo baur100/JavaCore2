6a55723f9a85802a3e17c74b07f328a533cbf385
ANNASENA <annadsena@gmail.com>
2020-03-27 19:49:20 -0700

    cv1

--- src/main/java/hWA7/Sum.java
+++ src/main/java/hWA7/Sum.java
@@ -2,7 +2,7 @@ package hWA7;
 
 public class Sum {
     public static int sum(int[] list1) {
-        int summa;
+        int summa =0;
         for (int x : list1) {
             summa = summa + x;
         }
