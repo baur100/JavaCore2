0b3d9f457a1c05a316a7c7acf7cda761582676fc
Alyona T <onehappywoman0509@gmail.com>
2020-03-27 18:48:25 -0700

    all

---
+++ src/main/java/Homework7/Ex1.java
@@ -0,0 +1,15 @@
+package Homework7;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int array = sum(2, 3, 5, 6, 7, 8);
+        System.out.println(array);
+    }
+
+    public static int sum(int a, int b, int c, int d, int e, int f) {
+        int sum1 = a + b + c + d + e + f;
+        return sum1;
+    }
+}
+
+
