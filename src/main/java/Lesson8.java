dde3ea773a8f78b60f83afff7d4783cbc7cbcfd0
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-03-27 23:24:53 -0400

    Lesson 8

--- src/main/java/Lesson8.java
+++ src/main/java/Lesson8.java
@@ -1,4 +1,5 @@
 public class Lesson8 {
+<<<<<<< HEAD
 
     public static void main(String[] args) {
         System.out.println(sum(1, 2));
@@ -30,3 +31,43 @@ public class Lesson8 {
 
     }
 }
+=======
+    public static void main(String[] args) {
+        System.out.println(sum(1,2));
+        int xx = sum(1,3,2);
+        int zz = sum(1,1,2,2);
+        String str = sum("abc","cde", "ddd");
+        System.out.println(str);
+        System.out.println(sum("ddd","eee",555));
+        System.out.println(sum("ddd","555"));
+    }
+
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static double sum(double x, double z){
+        return x+z;
+    }
+    public static int sum(int a, int b, int c){
+        return a+b+c;
+    }
+    public static double sum(double x, double z, double y){
+        return x+z+y;
+    }
+    public static int sum(int a,int b, int c,int d){
+        return a+b+c+d;
+    }
+    public static double sum(double x, double z, double y, double a){
+        return x+z+y +a;
+    }
+    public static String sum(String a, String b){
+        return a+b;
+    }
+    public static String sum(String a, String b, String c){
+        return a+b+c;
+    }
+    public static String sum(String a, String b, int c){
+        return a+b+c;
+    }
+}
+>>>>>>> 5c420ba... Lesson 8
