ea2f511adffc8bc22416a2f086eb72d2e330835a
DANILA PASTUKHOV <daniel1pastukhov@gmail.com>
2020-03-27 23:26:04 -0400

    HW7

--- src/main/java/hw7/HW72.java
+++ src/main/java/hw7/HW72.java
@@ -1,4 +1,8 @@
 package hw7;
 
 public class HW72 {
+//    public static int sort(int arr1[]){
+//
+//
+//    }
 }
--- src/main/java/hw7/Work.java
+++ src/main/java/hw7/Work.java
@@ -1,6 +1,7 @@
 package hw7;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import static hw7.HW71.sumOfArray;
 
@@ -11,9 +12,24 @@ public class Work {
         System.out.println("Array numbers #1 is " + Arrays.toString(arr1));
         System.out.println("Sum numbers of Array #1 is " + sumOfArray(arr1));
 
+        int[] a = {10,20,30,40,50};
+        int sum = IntStream.of(a).sum();
+        System.out.println("The sum is " + sum);
+
+        int[] numbers = {1,2,3,4,6,6,7};
+        int sum1 = 0;
+        for( int i : numbers) {
+            sum1+= i;
+        }
+        System.out.println(sum1);
+
         System.out.println("Exercise 2 ");
+
+        }
+
 
 
 
     }
-}
+
+
