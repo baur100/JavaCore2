8dec1ca24c5a23e9d43dd076b3ea342fc7d21cbc
Nikolay Vakolyuk <nvakoliouk@gmail.com>
2020-03-27 20:25:06 -0700

    L5

---
+++ src/main/java/Lesson8.java
@@ -0,0 +1,31 @@
+public class Lesson8 {
+
+    public static void main(String[] args) {
+        System.out.println(sum(1, 2));
+        int xx = sum(1, 2, 3);
+        int zz = sum(1, 2, 3, 2);
+        String str = sum("abc", "cde", "ddd");
+        System.out.println(str);
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static double sum(double x, double y, double z) {
+        return x + y + z;
+    }
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static int sum(int a, int b, int c, int d) {
+        return a + b + c + d;
+
+    }
+
+    public static String sum(String a, String b, String c){
+        return a + b + c;
+    }
+}
