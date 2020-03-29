25ab0df218e951b2683ca8e971ff0157537efa87
UmidaM <mukarramova@gmail.com>
2020-03-29 15:38:56 -0500

    HW5

--- src/main/java/HW5/HW5.java
+++ src/main/java/HW5/HW5.java
@@ -1,19 +1,19 @@
 package HW5;
 
-import javax.crypto.spec.PSource;
-
-import static java.lang.Integer.sum;
-
 public class HW5 {
+
+
     public static void main(String[] args) {
+        System.out.println(isOdd(88));
 
-
-//        int [] num1 = {10,20};
-//        System.out.println (sum(num1));          I got "expected 2 args but found 1". Why?
+        int[]num1 = {10,20};
+        System.out.println (sum(num1[0],num1[1]));
 
         int a = 30;
         int b = 40;
-        System.out.println(a + b);
+        System.out.println (sum(a,b));
+
+
     }
 
     public static int sum(int a, int b) {
@@ -21,16 +21,16 @@ public class HW5 {
     }
 
 
-//    public static boolean isOdd(int x) {
-//        if (x % 2 == 0) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-//        System.out.println(isOdd(88))             Noted as error! 
-//
-//    }
-//}
+    public static boolean isOdd(int x) {
+        if (x % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+}
 
 
 
