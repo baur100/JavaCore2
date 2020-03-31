09242fa14fb1027b94183369b97e941ab9e11bba
Dinara <dmcmoore21@gmail.com>
2020-03-30 20:14:20 -0700

    hW

--- src/main/java/HomeWork/HW_7.java
+++ src/main/java/HomeWork/HW_7.java
@@ -3,6 +3,20 @@ package HomeWork;
 public class HW_7 {
     public static void main(String[] args) {
 
+        int [] arr = {2,3,5,6,7,8};
+        System.out.println(sum(arr));
+
+    }
+    public static int sum (int [] array){
+        int sum = 0;
+        for (int v : array){
+            sum = sum+v;
+        }
+        return sum;
+    }
+
+    public static void sort (){
 
     }
 }
+
