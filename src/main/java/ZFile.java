efff65ae9eb95cd0965bcbdd2b0b8e9a2043c77d
Zakiya AG <zakiya.aghayeva@gmail.com>
2020-03-30 22:22:20 -0500

    just  notes

--- src/main/java/ZFile.java
+++ src/main/java/ZFile.java
@@ -1,2 +1,10 @@
 public class ZFile {
+    public static void main(String[] args) {
+
+        int[] x = {4,5,6,71,2,3};
+        x[6] = 22;
+        for (int v : x) {
+            System.out.print(v);
+        }
+    }
 }
