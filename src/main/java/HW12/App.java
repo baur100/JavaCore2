51c93e6a851ab40d6e9a7244b4cdc0c21ebe6add
Natalia Pak <natalia.nam1981@gmail.com>
2020-04-04 16:34:15 -0700

    HW11

---
+++ src/main/java/HW12/App.java
@@ -0,0 +1,13 @@
+package HW12;
+
+public class App {
+    public static void main(String[] args) {
+//        1. Создать один супер и 2 или 3 детских
+//        создать объекты
+//
+//
+//        2. Создать объекты с глубиной в 3 класса  создать объект и вытащить объект при помощи геттеров
+//                println
+//        comp.getSysBlock().getProcessor().getBrand()
+    }
+}
---
+++ src/main/java/HW12/Furniture.java
@@ -0,0 +1,7 @@
+package HW12;
+
+public class Furniture {
+    
+
+
+}
