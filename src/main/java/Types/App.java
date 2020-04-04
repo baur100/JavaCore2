7306b984344ae134743380d46019a6c673ddd015
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-04-03 22:58:40 -0400

    L12

--- src/main/java/Types/App.java
+++ src/main/java/Types/App.java
@@ -1,5 +1,6 @@
 package Types;
 
+<<<<<<< HEAD
 public class App {
     public static void main(String[] args) {
         String str = " I am string";
@@ -10,5 +11,35 @@ public class App {
         p.name = "Boing";
         System.out.println(p.getInfo());
 
+=======
+import java.util.Date;
+import java.util.Random;
+
+public class App {
+
+    public static void main(String[] args) {
+        String str = " I'm a string";
+        int a = 5;
+        System.out.println(a);
+        Plane p = new Plane();
+        p.name="Boing";
+
+        System.out.println(p.getInfo());
+
+        int[] arr = {1,1,3};
+
+        for (int v:arr) {
+            System.out.println(v);
+        }
+        System.out.println(p);
+
+        Date xx = new Date();
+        Random random = new Random();
+
+        System.out.println(xx);
+
+
+
+>>>>>>> 7688eb0... L12
     }
 }
--- src/main/java/Types/Plane.java
+++ src/main/java/Types/Plane.java
@@ -3,7 +3,17 @@ package Types;
 public class Plane {
     public String name;
 
+<<<<<<< HEAD
     public String getInfo() {
       return name;
+=======
+    @Override
+    public String toString() {
+        return "name=" + name;
+    }
+
+    public String getInfo(){
+        return name;
+>>>>>>> 7688eb0... L12
     }
 }
