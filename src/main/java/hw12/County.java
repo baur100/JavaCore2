4738831857d532f6b7241460272416adb4866c08
Anton Rosh <rosh8442@gmail.com>
2020-04-04 12:13:00 -0700

    HW12-1 updates

--- src/main/java/hw12/County.java
+++ src/main/java/hw12/County.java
@@ -4,6 +4,18 @@ public class County extends City {
     public String nameCounty;
     public String zipCode;
 
+    public County(String country, String state, int population, String capitol, String time, String metropolia, String mayor) {
+        super(country, state, population, capitol, time, metropolia, mayor);
+    }
+
+    public void setNameCounty(String nameCounty) {
+        this.nameCounty = nameCounty;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
     public County(String country, String state, int population, String capitol,
                   String time, String metropolia, String mayor, String nameCounty, String zipCode) {
         super(country, state, population, capitol, time, metropolia, mayor);
