be0218d1890d411659ee70445b8aa257f14f0e09
DANILA PASTUKHOV <daniel1pastukhov@gmail.com>
2020-04-07 20:02:16 -0400

    HW15

---
+++ src/main/java/homewor15/Colors.java
@@ -0,0 +1,4 @@
+package homewor15;
+
+public class Colors {
+}
--- src/main/java/homewor15/Work.java
+++ src/main/java/homewor15/Work.java
@@ -3,8 +3,53 @@ package homewor15;
 //добавьте в каждый ArrayList по 4 элемента
 //попробуйте методы add/set/remove/get/foreach
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Work {
     public static void main(String[] args) {
-        
+        List<String> colors = new ArrayList<>();
+        String[] colors1 = new String[10];
+// Добавление элементов "add"
+        colors.add("RED");
+        colors.add("Pink");
+        colors.add("Yellow");
+        colors.add("Green");
+        colors.add("PINK");
+        colors.add("RED");
+        colors.add("Pink");
+        colors.add("Yellow");
+        colors.add("Green");
+        colors.add("PINK");
+
+
+        System.out.println(colors);
+
+// Замена элемента 4 с GREEN на WHITE "set"
+        colors.set(4, "WHITE");
+        System.out.println(colors);
+
+//  Удаление элемента по индексу "remove"
+        colors.remove(2);
+        System.out.println(colors);
+
+//  Удаление элемента по содержимому(имени или значению) "remove"
+        colors.remove("Pink");
+        System.out.println(colors);
+
+// Эллемент добавляется в конец списка
+        colors.add("FUXI");
+        System.out.println(colors);
+
+// Есть ли в списке такое значение
+
+        System.out.println(colors.contains("Green"));
+        System.out.println(colors.contains("Oh-ho-ho"));
+
+// Напечатать размер массива
+        System.out.println(colors.size());
+
+
     }
 }
