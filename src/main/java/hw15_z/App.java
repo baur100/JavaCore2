f1950ac3d08c37925752277e46ec9f394603fa42
Zakiya AG <zakiya.aghayeva@gmail.com>
2020-04-08 12:40:03 -0500

    HW 15

--- src/main/java/hw15_z/App.java
+++ src/main/java/hw15_z/App.java
@@ -2,9 +2,20 @@ package hw15_z;
 
 import java.util.ArrayList;
 
+
 public class App {
     public static void main(String[] args) {
         ArrayList<String> transport = new ArrayList<String>();
+        String[] arrData = {"Bus", "Metro", "Trolleybus", "Yacht"};
+        System.out.println("\nI am using Foreach to print this: ");
+        for (String strTemp : arrData) System.out.println(strTemp);
+
+        transport.add("Bus");
+        transport.add("Metro");
+        transport.add("Trolleybus");
+        transport.add("Yacht");
+        System.out.println("List of transportation ");
+
 
         transport.add("bus");
         System.out.println(transport);
@@ -12,21 +23,29 @@ public class App {
         System.out.println(transport);
         transport.add("train");
         System.out.println(transport);
-        transport.add(2,"airplaine");
+        transport.add(2, "airplaine");
         System.out.println(transport);
-        transport.set(1,"taxi cab");
+        transport.set(1, "taxi cab");
         System.out.println(transport);
 //        System.out.println(transport.get(1));
         transport.remove(3);
         System.out.println(transport);
         transport.add("boat");
         transport.add("ferry");
-        transport.get(4);
+//        transport.get(4);
+        String element = transport.get(2);
+        System.out.println("the element at index 2 is " +element);
+
         System.out.println(transport);
         System.out.println(transport.get(4));
-        transport.forEach();
+//        transport.forEach((n)-> (transport));
+        System.out.println("Popular transportation modes are: " + transport);
+//        String v;
+//        transport.forEach(String v:transport){
+//            System.out.println(v);
 
-//        System.out.println(transport.get(2));
-//        System.out.println(transport.size());
+
+            System.out.println(transport.get(2));
+            System.out.println(transport.size());
+        }
     }
-}
