09bcec0e7e1ad5e6981ebaaa6e4f6022de02f46f
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-04-08 22:54:02 -0400

    L16 Array

---
+++ src/main/java/chw15/App.java
@@ -0,0 +1,12 @@
+package chw15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        var p1 = new Person("Vlad");
+        people.add(p1);
+    }
+}
