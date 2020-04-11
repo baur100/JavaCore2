c210b2e858f216d7c63a969210d46e2d65c699d4
Nikolay Vakolyuk <nvakoliouk@gmail.com>
2020-04-10 23:53:19 -0700

    l17

--- src/main/java/Lesson17/App.java
+++ src/main/java/Lesson17/App.java
@@ -1,21 +1,32 @@
 package Lesson17;
 
 public class App {
-    public static void main(String[] args){
-
-        int[] xx = {2, 54, 2};
+    public static void main(String[] args) {
+        int[] xx = {2, 5, 1};
         try {
-            int vv = xx[5];
-            int vvv = 6/0;
+            int vv = xx[1];
+            int vvv = 6 / 0;
 
             System.out.println("not error");
-        } catch (Exception err) {
+        } catch (ArrayIndexOutOfBoundsException err) {
             System.out.println("error");
+
+        } catch (ArithmeticException err) {
             System.out.println("Divide by 0");
 
-        }finally {
-            System.out.println("any case");
+        } catch (Exception err) {
+            System.out.println("Unknown exeption");
+            // throw new Exception("Errrrr");
 
+        } finally {
+            System.out.println("any run case");
         }
+        var p = new Person("Ivan", "Krotov");
+        var p2 = new Person("Ivan1", "Krotov2");
+
+        Person.counter = 10;
+        //Person.setCounter(15);
+        //System.out.println(Person.getCounter());
+
     }
 }
---
+++ src/main/java/Lesson17/Person.java
@@ -0,0 +1,13 @@
+package Lesson17;
+
+public class Person {
+    private String name;
+    private String lastname;
+    public static int counter;
+
+    public Person(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+
+    }
+}
