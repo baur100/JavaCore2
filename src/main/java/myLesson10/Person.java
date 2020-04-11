f92a3b56485c65589d8a5c361c8bbe0a380e5fd6
antonrosh <2qmVACuadGmGyCvQ8tGm>
2020-04-11 09:35:38 -0700

    Lesson 17 Try - Catch Exception Error TestNG

--- src/main/java/myLesson10/Person.java
+++ src/main/java/myLesson10/Person.java
@@ -9,6 +9,12 @@ public class Person {
         this.lastName = lastName;
     }
 
-    ;
+    public String result(int a, int b, int c) {
+        return firstName + (a + b + c);
+    }
+
+    public String lastResult(int a, String b) {
+        return lastName + a + b;
+    }
 
 }
---
+++ src/test/java/TestMyLesson10Person.java
@@ -0,0 +1,29 @@
+import myLesson10.Person;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestMyLesson10Person {
+    @Test
+    public void testResultFromPerson() {
+        // Arrange
+        Person person = new Person("Ivan", "Krotov");
+
+        // Act
+        String res = person.result(2, 1, 1);
+
+        // Assert
+        Assert.assertEquals(res, "Ivan4");
+    }
+
+    @Test
+    public void testLastResultMethodFromPersonInLesson10() {
+        // Arrange
+        Person doris = new Person("Dorris", "Sun");
+
+        //Act
+        String res = doris.lastResult(8, "Finish");
+
+        //Assert
+        Assert.assertEquals(res, "Sun8Finish");
+    }
+}
