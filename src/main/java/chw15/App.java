a33f6af954daeee73a83b2bed1e68948865e06d1
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-04-10 23:04:07 -0400

    L17 Static, Try/Catch, UnitTests

--- src/main/java/chw15/App.java
+++ src/main/java/chw15/App.java
@@ -1,12 +1,23 @@
 package chw15;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
         var p1 = new Person("Vlad");
         people.add(p1);
+
+        List<Fruit> fruits = new ArrayList<>();
+        fruits.add(new Fruit("Banana"));
+        fruits.add(new Fruit("Apple"));
+
+        Map<Person,Fruit> iLoveFruit = new HashMap<>();
+        iLoveFruit.put(new Person("Ivan"),new Fruit("Peach"));
+
+
     }
 }
