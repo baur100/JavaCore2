5a02858fcfe6cd4b5f18a83f3c18f63958939a91
Sergey Smith <shsmith@bk.ru>
2020-04-11 23:46:14 -0400

    Merge remote-tracking branch 'remotes/origin/master' into sh11
    # Conflicts:
    #	src/main/java/L9/App.java

--- src/main/java/HW14/work.java
+++ src/main/java/HW14/work.java
@@ -1,11 +1,13 @@
 package HW14;
-
+import HW14.carnivoresPack.lion;
+import HW14.carnivoresPack.blueWhale;
+import HW14.carnivoresPack.wolf;
 public class work {
     public static void main(String[] args) {
 
-        HW14.carnivoresPack.blueWhale newWhale = new HW14.carnivoresPack.blueWhale();
-        HW14.carnivoresPack.wolf newWolf = new HW14.carnivoresPack.wolf();
-        HW14.carnivoresPack.lion newLion = new HW14.carnivoresPack.lion();
+        blueWhale newWhale = new HW14.carnivoresPack.blueWhale();
+        wolf newWolf = new HW14.carnivoresPack.wolf();
+        lion newLion = new HW14.carnivoresPack.lion();
 
         newWhale.canLiveBearing();
         newWolf.canBite();
