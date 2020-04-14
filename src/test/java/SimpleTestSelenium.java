5cb0d845e3e2c6950f5cd91a54399060ce75f9a5
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-04-13 23:19:23 -0400

    S1

--- src/test/java/SimpleTestSelenium.java
+++ src/test/java/SimpleTestSelenium.java
@@ -1,9 +1,13 @@
 import org.openqa.selenium.By;
+<<<<<<< HEAD
 import org.openqa.selenium.Keys;
+=======
+>>>>>>> 3236347... S1
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+<<<<<<< HEAD
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -56,3 +60,28 @@ public class SimpleTestSelenium {
         searchField.sendKeys(Keys.RETURN);
     }
 }
+=======
+import org.testng.annotations.Test;
+
+public class SimpleTestSelenium {
+    @Test
+    public void googleSearch_OpenGooglePage_SearchPageOpened() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        WebElement searchField = driver.findElement(By.xpath("//*[@name='q']"));
+        searchField.sendKeys("selenium");
+
+        WebElement searchButton = driver.findElement(By.xpath("//*[@name='btnK']"));
+        searchButton.click();
+
+        Assert.assertEquals(driver.getTitle(),"selenium - Google Search");
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
+>>>>>>> 3236347... S1
