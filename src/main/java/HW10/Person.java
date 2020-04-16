a991cae4db0e3dbe58968cf8f2eefb5301f5a72d
UmidaM <mukarramova@gmail.com>
2020-04-15 23:08:35 -0500

    HW10Person

---
+++ src/main/java/HW10/Person.java
@@ -0,0 +1,15 @@
+package HW10;
+
+public class Person {
+    public String name;
+    public String lastname;
+
+    public Person(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public String result (int a, int b, int c) {
+        return name+(a+b+c);
+    }
+}
