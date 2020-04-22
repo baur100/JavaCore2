860ef2136899ad34f182d2c44a9e51f6be9ec4cb
Vlad Vitel <vladyslavvitel@gmail.com>
2020-04-21 23:06:48 -0400

    extra 1

--- src/test/java/KoelSimpleTest.java
+++ src/test/java/KoelSimpleTest.java
@@ -1,10 +1,9 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,9 +24,10 @@ public class KoelSimpleTest {
         explicitWait = new WebDriverWait(driver, 10);
         fluentWait = new FluentWait<WebDriver>(driver)
                 .withTimeout(Duration.ofSeconds(10))
-                .pollingEvery(Duration.ofMillis(100))
+                .pollingEvery(Duration.ofMillis(700))
                 .ignoring(Exception.class)
-                .ignoring(StaleElementReferenceException.class);
+                .ignoring(StaleElementReferenceException.class)
+                .ignoring(ElementClickInterceptedException.class);
 
 //        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
@@ -39,7 +39,37 @@ public class KoelSimpleTest {
         Thread.sleep(5000);
         driver.quit();
     }
-    @Test
+
+    @Test(enabled = true)
+    public void loginKoel() throws InterruptedException {
+        var email = driver.findElement(By.xpath("//*[@type='email']"));
+        email.sendKeys("testpro.user02@testpro.io");
+
+        var password = driver.findElement(By.xpath("//*[@type='password']"));
+        password.sendKeys("te$t$tudent02");
+
+        var loginbutton = driver.findElement(By.xpath("//*[@type='submit']"));
+        loginbutton.click();
+
+//        Thread.sleep(2000);
+        fluentWait.until(driver -> driver.findElement(By.xpath("//*[@class='fa fa-plus-circle control create']")).isDisplayed());
+
+
+//        explicitWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='fa fa-plus-circle control create']")));
+//        Thread.sleep(2000);
+        var creatplaylist = driver.findElement(By.xpath("//*[@class='fa fa-plus-circle control create']"));
+        creatplaylist.click();
+
+        WebElement searchField = driver.findElement(By.xpath("//*[@type='text'][1]"));
+        searchField.sendKeys("Denisplaylist");
+        searchField.sendKeys(Keys.ENTER);
+
+        var logout = driver.findElement(By.xpath("//*[@class='fa fa-sign-out control']"));
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://koelapp.testpro.io/");
+    }
+
+    @Test(enabled = false)
     public void login_CorrectCredentials_LoggedToApp(){
         var email = driver.findElement(By.xpath("//*[@type='email']"));
         email.sendKeys("testpro.user02@testpro.io");
@@ -53,7 +83,7 @@ public class KoelSimpleTest {
         var homeButton = driver.findElement(By.xpath("//*[@class='home active']"));
         homeButton.click();
     }
-    @Test
+    @Test(enabled = false)
     public void login_CorrectCredentialsExplicitWait_LoggedToApp(){
         var email = driver.findElement(By.xpath("//*[@type='email']"));
         email.sendKeys("testpro.user02@testpro.io");
@@ -69,7 +99,7 @@ public class KoelSimpleTest {
         homeButton.click();
     }
 
-    @Test
+    @Test(enabled = false)
     public void login_CorrectCredentialsFluent_LoggedToApp(){
         var email = driver.findElement(By.xpath("//*[@type='email']"));
         email.sendKeys("testpro.user02@testpro.io");
