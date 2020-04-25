970b2991ca843070a6124dc90d896a778f9edb0b
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-04-24 23:02:36 -0400

    S8

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -1,8 +1,11 @@
 package pageObjects;
 
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -10,13 +13,16 @@ public class BasePage {
     protected WebDriver driver;
     protected FluentWait<WebDriver> fluentWait;
     protected final String url = "https://koelapp.testpro.io/";
+    protected WebDriverWait explicitWait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+        explicitWait = new WebDriverWait(driver, 10);
         this.fluentWait = new FluentWait<WebDriver>(this.driver)
                 .withTimeout(Duration.ofSeconds(10))
                 .pollingEvery(Duration.ofMillis(100))
-                .ignoring(Exception.class)
-                .ignoring(StaleElementReferenceException.class);
+                .ignoring(ElementClickInterceptedException.class)
+                .ignoring(StaleElementReferenceException.class)
+                .ignoring(NoSuchElementException.class);
     }
 }
