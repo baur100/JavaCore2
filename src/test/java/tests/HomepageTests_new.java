90b381938c408d0529ed1f74ffdd42574041a765
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-04-28 07:19:53 -0400

    l10

--- src/test/java/tests/HomepageTests_new.java
+++ src/test/java/tests/HomepageTests_new.java
@@ -5,9 +5,9 @@ import org.testng.annotations.Test;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 
-public class HomepageTests extends BaseTest{
-    @Test//(enabled=false)
-    public void loginTest_createNewPlaylist_newPlaylistCreated() {
+public class HomepageTests_new extends BaseTest{
+    @Test
+    public void new_loginTest_createNewPlaylist_newPlaylistCreated() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.openPage();
         HomePage homePage = loginPage.login("testpro.user02@testpro.io","te$t$tudent02");
@@ -15,7 +15,7 @@ public class HomepageTests extends BaseTest{
         Assert.assertTrue(homePage.isPlaylistCreated("HeyYoPlaylist"));
     }
     @Test
-    public void scrollDown(){
+    public void new_scrollDown(){
         LoginPage loginPage = new LoginPage(driver);
         loginPage.openPage();
         HomePage homePage = loginPage.login("testpro.user02@testpro.io","te$t$tudent02");
