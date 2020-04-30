b2d71114863e0ea33fb8e8a00fdd79f76902059c
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-04-29 23:07:21 -0400

    l10

--- src/test/java/tests/HomepageTests.java
+++ src/test/java/tests/HomepageTests.java
@@ -1,28 +1,32 @@
 package tests;
 
+import listeners.RetryAnalyzer;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 
 public class HomepageTests extends BaseTest{
-    @Test//(enabled=false)
-    public void loginTest_createNewPlaylist_newPlaylistCreated() {
+    @Parameters({"email","password","playlist"})
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void loginTest_createNewPlaylist_newPlaylistCreated(String login, String pwd, String playlist) {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.openPage();
-        HomePage homePage = loginPage.login("testpro.user02@testpro.io","te$t$tudent02");
-        homePage.createNewPlaylist("HeyYoPlaylist");
-        Assert.assertTrue(homePage.isPlaylistCreated("HeyYoPlaylist"));
+        HomePage homePage = loginPage.login(login,pwd);
+        homePage.createNewPlaylist(playlist);
+        Assert.assertTrue(homePage.isPlaylistCreated(playlist));
     }
+    @Parameters({"email","password","playlist", "new-playlist"})
     @Test
-    public void scrollDown(){
+    public void scrollDown(String login, String pwd, String playlist, String newPlaylist){
         LoginPage loginPage = new LoginPage(driver);
         loginPage.openPage();
-        HomePage homePage = loginPage.login("testpro.user02@testpro.io","te$t$tudent02");
-        homePage.createNewPlaylist("zzHeyYoPlaylist");
-        homePage.leftHandScrollDown("zzHeyYoPlaylist");
-        homePage.renamePlayList("zzHeyYoPlaylist","newName");
-        Assert.assertTrue(homePage.isPlaylistCreated("newName"));
+        HomePage homePage = loginPage.login(login,pwd);
+        homePage.createNewPlaylist(playlist);
+        homePage.leftHandScrollDown(playlist);
+        homePage.renamePlayList(playlist,newPlaylist);
+        Assert.assertTrue(homePage.isPlaylistCreated(newPlaylist));
 
     }
 }
