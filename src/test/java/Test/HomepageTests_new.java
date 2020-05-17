d28e230a3991e73b060bbc984f7193840b81c5fb
Zakiya AG <zakiya.aghayeva@gmail.com>
2020-05-16 23:12:23 -0500

    api

--- src/test/java/Test/HomepageTests_new.java
+++ src/test/java/Test/HomepageTests_new.java
@@ -1,12 +1,12 @@
-package tests;
+package test;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 
-public class HomepageTests_new extends BaseTest{
-    @Test
+public class HomepageTests_new extends BaseTest {
+    @Test 
     public void new_loginTest_createNewPlaylist_newPlaylistCreated() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.openPage();
@@ -19,10 +19,10 @@ public class HomepageTests_new extends BaseTest{
         LoginPage loginPage = new LoginPage(driver);
         loginPage.openPage();
         HomePage homePage = loginPage.login("testpro.user02@testpro.io","te$t$tudent02");
-        homePage.createNewPlaylist("zzHeyYoPlaylist");
-        homePage.leftHandScrollDown("zzHeyYoPlaylist");
-        homePage.renamePlayList("zzHeyYoPlaylist","newName");
-        Assert.assertTrue(homePage.isPlaylistCreated("newName"));
+        homePage.createNewPlaylist("Zelda");
+        homePage.leftHandScrollDown("Zelda");
+        homePage.renamePlayList("Zelda","newZelda");
+        Assert.assertTrue(homePage.isPlaylistCreated("newZelda"));
 
     }
 }
--- src/test/java/Test/KoelTestLogin.java
+++ src/test/java/Test/KoelTestLogin.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
