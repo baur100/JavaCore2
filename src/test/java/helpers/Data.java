a6e82af218ea8196ba3fa6d1a7db2d2d18bc7f45
Baurzhan Zh <baurzhan.zh@gmail.com>
2020-05-25 23:13:21 -0400

    L9

--- src/test/java/helpers/Data.java
+++ src/test/java/helpers/Data.java
@@ -1,7 +1,11 @@
 package helpers;
 
+import com.google.gson.Gson;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import models.CreatePlaylistRequest;
+import models.CreatePlaylistResponse;
 import models.DataResponse;
 
 import static io.restassured.RestAssured.given;
@@ -22,5 +26,27 @@ public class Data {
 
         JsonPath jsonPath = response.jsonPath();
         return jsonPath.getObject("$", DataResponse.class);
+    }
+    public static CreatePlaylistResponse createPlaylist(String playlistName){
+        String token = Token.retrieveToken("testpro.user02@testpro.io","te$t$tudent02");
+        String[] rules = {};
+        var playlist = new CreatePlaylistRequest(playlistName,rules);
+        var requestBody = new Gson().toJson(playlist);
+
+        Response response = given()
+                .baseUri("https://koelapp.testpro.io/")
+                .header("Authorization","Bearer "+token)
+                .basePath("api/playlist")
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        JsonPath jsonPath = response.jsonPath();
+        return jsonPath.getObject("$",CreatePlaylistResponse.class);
     }
 }
---
+++ src/test/java/models/RenamePlaylistResponse.java
@@ -0,0 +1,6 @@
+package models;
+
+public class RenamePlaylistResponse {
+    public int id;
+    public String name;
+}
